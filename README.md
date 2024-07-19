06a2240788182771f7440a771a5c10c539c06d2f
djokobozinov <djoko.bozinov@gmail.com>
2024-07-19 12:21:04 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Install MongoDB Database Tools
+https://www.mongodb.com/try/download/database-tools
+
+# Backup
+- Create .env file 
+```
+MONGODB_URI=mongodb://localhost:27017/example
+```
+- run backup script
+```
+npm run backup
+```
---
+++ src/backup.ts
@@ -0,0 +1,27 @@
+import { spawn } from 'child_process';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoUri = process.env.MONGODB_URI;
+
+if (!mongoUri) {
+  console.error('MongoDB URI is not defined.');
+  process.exit(1);
+}
+
+const backupProcess = spawn('mongodump', [
+  `--uri=${mongoUri}`,
+]);
+
+backupProcess.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+backupProcess.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+backupProcess.on('close', (code) => {
+  console.log(`mongodump process exited with code ${code}`);
+});
