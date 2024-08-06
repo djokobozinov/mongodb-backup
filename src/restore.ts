8be8dd15e3e40e0553fd6e8535b2a613c19c2a78
djokobozinov <djoko.bozinov@gmail.com>
2024-08-06 10:47:24 +0200

    Restore improvements.

--- src/restore.ts
+++ src/restore.ts
@@ -4,13 +4,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// MongoDB URI and backup directory from environment variables
+const backupDirName = 'football';
+
 const mongoUri = 'mongodb://localhost:27017/football-test';
-const backupDir = path.join(__dirname, '/dump/football');
+const rootPath = path.resolve(__dirname, '..');
+const backupDir = path.join(rootPath, 'dump', backupDirName);
 console.log(backupDir);
 if (!mongoUri) {
-  console.error('MongoDB URI is not defined.');
-  process.exit(1);
+	console.error('MongoDB URI is not defined.');
+	process.exit(1);
 }
 
 const MONGORESTORE_PATH = 'mongorestore';
@@ -20,13 +22,13 @@ const cmd = `${MONGORESTORE_PATH} --uri=${mongoUri} --dir=${backupDir} --drop`;
 
 // Execute the mongorestore command
 exec(cmd, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Restore failed: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`Restore completed successfully: ${stdout}`);
+	if (error) {
+		console.error(`Restore failed: ${error.message}`);
+		return;
+	}
+	if (stderr) {
+		console.error(`stderr: ${stderr}`);
+		return;
+	}
+	console.log(`Restore completed successfully: ${stdout}`);
 });
